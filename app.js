620fa28cc1be3f7e0c56accfe269075f21a9e7ea
oezge sebisteri <sebisteri@outlook.com>
2021-02-17 17:35:48 +0100

    some changes

--- app.js
+++ app.js
@@ -24,27 +24,27 @@ const capitalized = (string) => string[0].toUpperCase() + string.slice(1).toLowe
 
 app.locals.title = `${capitalized(projectName)}- Generated with IronGenerator`;
 
-// // Middleware Setup
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
+// Middleware Setup
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-// // Express View engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'hbs');
-// app.use(express.static(path.join(__dirname, 'public')));
-// //app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+// Express View engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 // // Handles http requests (express is node js framework)
-// const cookieParser = require('cookie-parser');
-// const favicon = require('serve-favicon');
-// const mongoose = require('mongoose');
-// const logger = require('morgan');
-// const path = require('path');
+const cookieParser = require('cookie-parser');
+const favicon = require('serve-favicon');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
 
-// const app_name = require('./package.json').name;
-// const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+const app_name = require('./package.json').name;
+const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
 // 👇 Start handling routes here
 //
